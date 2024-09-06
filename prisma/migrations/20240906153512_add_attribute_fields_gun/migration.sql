384aa3b6ffffe049a7475d12a304f9fb2ebc4b48
Darryl Morley <darrylmorley.uk@gmail.com>
2024-09-06 16:40:05 +0100

    add migration for guntrader attribute fields

---
+++ prisma/migrations/20240906153512_add_attribute_fields_gun/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Gun" ADD COLUMN     "barrel_dimensions" TEXT,
+ADD COLUMN     "choke" TEXT,
+ADD COLUMN     "choke2" TEXT,
+ADD COLUMN     "orientation" TEXT,
+ADD COLUMN     "stock_dimensions" TEXT,
+ADD COLUMN     "trigger" TEXT;
